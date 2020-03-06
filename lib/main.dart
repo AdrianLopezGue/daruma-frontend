a0e5db65949ebf8aa4cf55cea55c86cdd5a6f4a0
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-06 18:27:39 +0100

    Flutter project created and signin with google done

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:daruma/pages/login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
