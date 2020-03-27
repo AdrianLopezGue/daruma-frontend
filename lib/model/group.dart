e06ad6f202f037ebc412a4ee4df9344fdc1198ab
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-27 14:00:54 +0100

    group model modified

--- lib/model/group.dart
+++ lib/model/group.dart
@@ -2,16 +2,23 @@
 class Group{
   String name;
   String currencyCode;
+  String idOwner;
 
-  Group({this.name, this.currencyCode});
+  Group({this.name, this.currencyCode, this.idOwner});
 
   Group copyWith(
     {String name, 
-    String currencyCode}
+    String currencyCode,
+    String idOwner}
   ){
     return Group(
       name: name  ?? this.name,
-      currencyCode: currencyCode ?? this.currencyCode
+      currencyCode: currencyCode ?? this.currencyCode,
+      idOwner: idOwner ?? this.idOwner,
     );
+  }
+
+  String convertToString(){
+    return '{ "name": "$this.name", "currencyCode": "$this.currencyCode", "idOwner": "$this.idOwner"}';   
   }
 }
