f29fa004536b801b9819f0e82e8b4a6b8c8db308
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-03-27 12:25:22 +0100

    oauth button converted to single widget

---
+++ lib/ui/widget/index.dart
@@ -0,0 +1,1 @@
+export 'oauth-login-button-widget.dart';
---
+++ lib/ui/widget/oauth-login-button-widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class OAuthLoginButton extends StatelessWidget {
+  final Function() onPressed;
+  final String text;
+  final String assetName;
+  final Color backgroundColor;
+
+  OAuthLoginButton(
+      {this.onPressed, this.text, this.assetName, this.backgroundColor});
+
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      splashColor: backgroundColor,
+      onPressed: onPressed,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage(assetName), height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
