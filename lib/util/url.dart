0a2088b9d337794c7f15612c665105e4c8567c6f
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2020-05-12 17:25:50 +0200

    Small change in parameterization

--- lib/util/url.dart
+++ lib/util/url.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_config/flutter_config.dart';
 
 class Url {
-  static String apiBaseUrl = FlutterConfig.get('HOST') + ":" + FlutterConfig.get('PORT');
+  static String apiBaseUrl = FlutterConfig.get('HOST');
   static String currencyListBaseUrl =
       'https://openexchangerates.org/api/currencies.json';
 }
